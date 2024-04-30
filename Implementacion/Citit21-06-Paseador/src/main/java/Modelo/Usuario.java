edeeb17972f1c704e63052def19f333abe7d8172
IbaiBuR <ibaibur@chessnibble.com>
2024-04-30 16:23:25 +0200

    Add constructor, getters and setters to Usuario

--- Implementacion/Citit21-06-Paseador/src/main/java/Modelo/Usuario.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Modelo/Usuario.java
@@ -4,4 +4,31 @@ public class Usuario {
     private int id;
     private String nombre;
     private TPlataforma plataforma;
+
+    public Usuario(int id, TPlataforma plataforma, String nombre)
+    {
+        this.id = id;
+        this.plataforma = plataforma;
+        this.nombre = nombre;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public TPlataforma getPlataforma()
+    {
+        return plataforma;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
 }
