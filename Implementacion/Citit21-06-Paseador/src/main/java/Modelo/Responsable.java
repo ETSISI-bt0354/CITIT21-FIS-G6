27e2b03f1445462faf6f7a7cc319947e7da202a6
Jorge Benjumea <jorge.benjumea@alumnos.upm.es>
2024-05-08 19:56:28 +0200

    Responsable --- Cuidador

--- Implementacion/Citit21-06-Paseador/src/main/java/Modelo/Responsable.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Modelo/Responsable.java
@@ -5,24 +5,34 @@ import java.util.List;
 public class Responsable extends Usuario{
 
     private List<Mascota> Mascota;
+    private List<Cuidador> Cuidador;
 
     public Responsable(int id, TPlataforma plataforma, String nombre) {
         super(id, plataforma, nombre);
     }
-
     public List<Mascota> getMascota() {
         return Mascota;
     }
-
     public void setMascota(List<Mascota> mascota) {
         Mascota = mascota;
     }
-
     public void addMascota (Mascota mascota) {
         Mascota.add(mascota);
     }
-
     public void eliminarMascota (Mascota mascota) {
         Mascota.remove(mascota);
+    }
+
+    public void setCuidador(List<Cuidador> cuidador) {
+        Cuidador = cuidador;
+    }
+    public List<Cuidador> getCuidador() {
+        return Cuidador;
+    }
+    public void addCuidador (Cuidador cuidador) {
+        Cuidador.add(cuidador);
+    }
+    public void removeCuidador (Cuidador cuidador) {
+        Cuidador.remove(cuidador);
     }
 }
