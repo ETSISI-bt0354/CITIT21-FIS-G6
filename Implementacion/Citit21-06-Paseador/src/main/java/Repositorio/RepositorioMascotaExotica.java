1bd6ea75987e22beca72588b815376bbc183df6f
IbaiBuR <ibai.burgos@alumnos.upm.es>
2024-05-14 16:47:13 +0200

    Complete RepositorioMascotaExotica

--- Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioMascotaExotica.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioMascotaExotica.java
@@ -1,5 +1,52 @@
 package Repositorio;
 
-public class RepositorioMascotaExotica
-{
+import Modelo.Exotico;
+import Modelo.Mascota;
+import Modelo.Responsable;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class RepositorioMascotaExotica {
+    private final List<Exotico> repositorio;
+    private final RepositorioResponsable responsables;
+    private int maxId;
+
+    public RepositorioMascotaExotica(RepositorioResponsable responsables) {
+        repositorio = new ArrayList<>();
+        this.responsables = responsables;
+        maxId = 0;
+    }
+
+    public Exotico crear(HashMap<String, String> exotico) {
+        int codigoPostal = Integer.parseInt(exotico.get("codigo-postal"));
+        Responsable responsable = responsables.obtener(Integer.parseInt(exotico.get("responsable")));
+        Path permiso = Paths.get(exotico.get("permiso"));
+        Path certificadoLegal = Paths.get(exotico.get("certificado-legal"));
+        Path certificadoSalud = Paths.get(exotico.get("certificado-salud"));
+        Exotico e = new Exotico(exotico.get("nombre"), codigoPostal, exotico.get("descripcion"), permiso, certificadoLegal, certificadoSalud);
+
+        repositorio.add(e);
+
+        return e;
+    }
+
+    public Mascota obtener(int id) {
+        return repositorio.stream().filter(exotico -> exotico.getId() == id).findAny().orElseThrow();
+    }
+
+    public void actualizar(Exotico exotico) {
+
+    }
+
+    public void borrar(int id) {
+        repositorio.remove(repositorio.stream().filter(exotico -> exotico.getId() == id).findAny().orElseThrow());
+    }
+
+    private int assignId() {
+        return maxId++;
+    }
 }
