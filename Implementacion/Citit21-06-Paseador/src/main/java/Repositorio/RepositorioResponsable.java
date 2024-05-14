f2e126fa18976180716013440902f7b5e935a909
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2024-05-14 12:43:49 +0200

    Responsables repo

--- Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioResponsable.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioResponsable.java
@@ -1,5 +1,56 @@
 package Repositorio;
 
+import Modelo.Responsable;
+import Modelo.TPlataforma;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class RepositorioResponsable
 {
+    private final List<Responsable> repositorio;
+    private int maxId;
+
+    public RepositorioResponsable()
+    {
+        repositorio = new ArrayList<>();
+        maxId = 0;
+    }
+
+    public Responsable crear(HashMap<String, String> cuidador)
+    {
+        TPlataforma plataforma = switch (cuidador.get("plataforma")) {
+            case "twitter" -> TPlataforma.TWITTER;
+            case "facebook" -> TPlataforma.FACEBOOK;
+            case "google" -> TPlataforma.GOOGLE;
+            case "microsoft" -> TPlataforma.MICROSOFT;
+            default -> throw new IllegalArgumentException("Plataforma no valida");
+        };
+
+        Responsable r = new Responsable(assingId(), plataforma, cuidador.get("nombre"));
+
+        repositorio.add(r);
+
+        return r;
+    }
+
+    public Responsable obtener(int id)
+    {
+        return repositorio.stream().filter(responsable -> responsable.getId() == id).findAny().orElseThrow();
+    }
+
+    public void actualizar(Responsable responsable) {
+
+    }
+
+    public void borrar(int id)
+    {
+        repositorio.remove(repositorio.stream().filter(responsable -> responsable.getId() == id).findAny().orElseThrow());
+    }
+
+    private int assingId()
+    {
+        return maxId++;
+    }
 }
