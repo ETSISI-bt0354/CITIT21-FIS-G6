39c1ac06b8cb7bdd98a1a8cd99c1677a67621472
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2024-05-14 12:44:21 +0200

    Mascotas repo

--- Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioMascota.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioMascota.java
@@ -1,5 +1,52 @@
 package Repositorio;
 
+import Modelo.Mascota;
+import Modelo.Responsable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class RepositorioMascota
 {
+    private final List<Mascota> repositorio;
+    private final RepositorioResponsable responsables;
+    private int maxId;
+
+    public RepositorioMascota(RepositorioResponsable responsables)
+    {
+        repositorio = new ArrayList<>();
+        this.responsables = responsables;
+        maxId = 0;
+    }
+
+    public Mascota crear(HashMap<String, String> mascota)
+    {
+        int codigoPostal = Integer.parseInt(mascota.get("codigo-postal"));
+        Responsable responsable = responsables.obtener(Integer.parseInt(mascota.get("responsable")));
+        Mascota r = new Mascota(assingId(), mascota.get("nombre"), codigoPostal, mascota.get("descripcion"), responsable);
+
+        repositorio.add(r);
+
+        return r;
+    }
+
+    public Mascota obtener(int id)
+    {
+        return repositorio.stream().filter(mascota -> mascota.getId() == id).findAny().orElseThrow();
+    }
+
+    public void actualizar(Mascota mascota) {
+
+    }
+
+    public void borrar(int id)
+    {
+        repositorio.remove(repositorio.stream().filter(mascota -> mascota.getId() == id).findAny().orElseThrow());
+    }
+
+    private int assingId()
+    {
+        return maxId++;
+    }
 }
