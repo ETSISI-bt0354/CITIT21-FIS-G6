87ed60361eec09ec0c96236fbe09e1b56ca79d26
IbaiBuR <ibai.burgos@alumnos.upm.es>
2024-05-14 15:38:46 +0200

    Add ControladorMascota placeholder

--- Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorMascota.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorMascota.java
@@ -1,5 +1,26 @@
 package Controlador;
 
+import Repositorio.RepositorioMascota;
+
+import java.util.HashMap;
+
 public class ControladorMascota
 {
+    private RepositorioMascota repositorio;
+
+    public ControladorMascota(RepositorioMascota repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public void crearCuidado(HashMap<String, String> params) {
+
+    }
+
+    public void crearCuidadoMascota(HashMap<String, String> params) {
+
+    }
+
+    public void crearCuidadoCuidador(HashMap<String, String> params) {
+
+    }
 }
