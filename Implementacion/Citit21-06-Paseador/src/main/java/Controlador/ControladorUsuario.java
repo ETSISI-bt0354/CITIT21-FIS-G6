43caecd4dc0886ee0deb424f0151a85cecdccb54
Jorge Benjumea <jorge.benjumea@alumnos.upm.es>
2024-05-14 15:43:25 +0200

    Hmm

--- Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorUsuario.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorUsuario.java
@@ -2,9 +2,12 @@ package Controlador;
 
 import Modelo.Cuidador;
 import Modelo.Responsable;
+import Modelo.TPlataforma;
 import Modelo.Usuario;
 import Repositorio.RepositorioCuidador;
 import Repositorio.RepositorioResponsable;
+
+import java.util.HashMap;
 
 public class ControladorUsuario
 {
