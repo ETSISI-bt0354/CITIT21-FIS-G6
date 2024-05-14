3a10c174dd8173ce57d18426cdd1f9b1d1d5eb3c
Jorge Benjumea <jorge.benjumea@alumnos.upm.es>
2024-05-14 15:37:26 +0200

    ContrlUser

--- Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorUsuario.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorUsuario.java
@@ -1,5 +1,28 @@
 package Controlador;
 
+import Modelo.Cuidador;
+import Modelo.Responsable;
+import Modelo.Usuario;
+import Repositorio.RepositorioCuidador;
+import Repositorio.RepositorioResponsable;
+
 public class ControladorUsuario
 {
+    private static Usuario loggedUser = null;
+    private RepositorioResponsable repositorioResponsable;
+    private RepositorioCuidador repositorioCuidador;
+
+    public ControladorUsuario(RepositorioResponsable repositorioResponsable, RepositorioCuidador repositorioCuidador) {
+        this.repositorioResponsable = repositorioResponsable;
+        this.repositorioCuidador = repositorioCuidador;
+    }
+
+    public void registrarCuidador(Cuidador cuidador) {
+    }
+
+    public void registrarResponsable(Responsable responsable){
+    }
+
+    public void mostrarUsuario(Usuario usuario) {
+    }
 }
