1ddf862f01b4cb6ab5dbd62343b25d0f8f63f831
IbaiBuR <ibai.burgos@alumnos.upm.es>
2024-05-14 16:47:40 +0200

    Add ControladorMascota

--- Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorMascota.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorMascota.java
@@ -1,5 +1,37 @@
 package Controlador;
 
-public class ControladorMascota
-{
+import Modelo.Mascota;
+import Repositorio.RepositorioMascota;
+import Vista.VistaMascota;
+
+import java.util.HashMap;
+
+public class ControladorMascota {
+    private static final int MIN_REGISTER_PARAMS = 4;
+    private RepositorioMascota repositorio;
+    private VistaMascota vista;
+
+    public ControladorMascota(RepositorioMascota repositorio) {
+        this.repositorio = repositorio;
+        this.vista = new VistaMascota();
+    }
+
+    public void registrarMascota(HashMap<String, String> params) {
+        if (params.size() < MIN_REGISTER_PARAMS)
+            throw new IllegalArgumentException("Faltan parametros para registrar la mascota.");
+
+        vista.registrarMascota(repositorio.crear(params));
+
+    }
+
+    public void registrarMascotaExotica(HashMap<String, String> params) {
+
+    }
+
+    public void listarMascotas() {}
+
+    public void mostrarMascota(Mascota mascota) {
+
+
+    }
 }
