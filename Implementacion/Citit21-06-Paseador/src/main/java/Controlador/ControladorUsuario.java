a708dbfdd39611e601a06f9e8dcf87c51268c5e5
Jorge Benjumea <jorge.benjumea@alumnos.upm.es>
2024-05-14 16:08:37 +0200

    There it goes

--- Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorUsuario.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Controlador/ControladorUsuario.java
@@ -1,5 +1,33 @@
 package Controlador;
 
+import Modelo.Cuidador;
+import Modelo.Responsable;
+import Modelo.Usuario;
+import Repositorio.RepositorioCuidador;
+import Repositorio.RepositorioResponsable;
+
+import java.util.HashMap;
+
 public class ControladorUsuario
 {
+    private static Usuario loggedUser = null;
+    private RepositorioResponsable repositorioResponsable;
+    private RepositorioCuidador repositorioCuidador;
+
+    public ControladorUsuario(RepositorioResponsable repositorioResponsable, RepositorioCuidador repositorioCuidador) {
+        this.repositorioResponsable = repositorioResponsable;
+        this.repositorioCuidador = repositorioCuidador;
+    }
+
+    public void registrarCuidador(HashMap<String, String> cuidador) {
+
+        repositorioCuidador.crear(cuidador);
+    }
+
+    public void registrarResponsable(HashMap<String, String> responsable) {
+        repositorioResponsable.crear(responsable);
+    }
+
+    public void mostrarUsuario(Usuario usuario) {
+    }
 }
