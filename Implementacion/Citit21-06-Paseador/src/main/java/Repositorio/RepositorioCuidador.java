3fb83989e4119133718cb94f864bfd043be58745
Jorge Benjumea <jorge.benjumea@alumnos.upm.es>
2024-05-14 15:23:16 +0200

    RepoCuidador (half-baked)

--- Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioCuidador.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioCuidador.java
@@ -1,5 +1,56 @@
 package Repositorio;
 
+import Modelo.Cuidador;
+import Modelo.TPlataforma;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class RepositorioCuidador
 {
+    private final List<Cuidador> repositorio;
+    private int maxId;
+
+    public RepositorioCuidador()
+    {
+        repositorio = new ArrayList<>();
+        maxId = 0;
+    }
+
+    public Cuidador crear(HashMap<String, String> cuidador)
+    {
+        TPlataforma plataforma = switch (cuidador.get("plataforma")) {
+            case "twitter" -> TPlataforma.TWITTER;
+            case "facebook" -> TPlataforma.FACEBOOK;
+            case "google" -> TPlataforma.GOOGLE;
+            case "microsoft" -> TPlataforma.MICROSOFT;
+            default -> throw new IllegalArgumentException("Plataforma no valida");
+        };
+
+        Cuidador c = new Cuidador(); //TODO
+
+        repositorio.add(c);
+
+        return c;
+    }
+
+    public Cuidador obtener(int id)
+    {
+        return repositorio.stream().filter(cuidador -> cuidador.getId() == id).findAny().orElseThrow();
+    }
+
+    public void actualizar(Cuidador cuidador) {
+
+    }
+
+    public void borrar(int id)
+    {
+        repositorio.remove(repositorio.stream().filter(cuidador -> cuidador.getId() == id).findAny().orElseThrow());
+    }
+
+    private int assignID()
+    {
+        return maxId++;
+    }
 }
