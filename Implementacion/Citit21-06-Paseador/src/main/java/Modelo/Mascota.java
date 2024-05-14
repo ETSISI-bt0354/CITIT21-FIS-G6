2fc520a17db2eed8ad3e19192b372c31fbb43830
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2024-05-14 12:43:25 +0200

    Add missing fields

--- Implementacion/Citit21-06-Paseador/src/main/java/Modelo/Mascota.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Modelo/Mascota.java
@@ -3,17 +3,26 @@ package Modelo;
 import java.util.List;
 
 public class Mascota implements IMascota {
+    private final int id;
     private String nombre;
     private int codigoPostal;
     private String descripcion;
     private List<Foto> fotos;
     private Perfil perfil;
     private List<Cuidado> cuidados;
+    private Responsable responsable;
 
-    public Mascota(String nombre, int codigoPostal, String descripcion) {
+    public Mascota(int id, String nombre, int codigoPostal, String descripcion, Responsable responsable) {
+        this.id = id;
         this.nombre = nombre;
         this.codigoPostal = codigoPostal;
         this.descripcion = descripcion;
+        this.responsable = responsable;
+    }
+
+    public int getId()
+    {
+        return id;
     }
 
     public String getNombre() {
@@ -74,6 +83,10 @@ public class Mascota implements IMascota {
     }
     public void removeCuidado(Cuidado cuidado) {
         this.cuidados.remove(cuidado);
+    }
+
+    public Responsable getResponsable() {
+        return responsable;
     }
 
 }
