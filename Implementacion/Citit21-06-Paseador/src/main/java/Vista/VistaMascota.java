4f20f2085953f11583fc57f8e13103e63b43f55a
Jorge Benjumea <jorge.benjumea@alumnos.upm.es>
2024-05-15 11:31:31 +0200

    🎨 The color update

--- Implementacion/Citit21-06-Paseador/src/main/java/Vista/VistaMascota.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Vista/VistaMascota.java
@@ -3,20 +3,27 @@ package Vista;
 import Modelo.IMascota;
 
 public class VistaMascota {
-  public void registrarMascota(IMascota mascota) {
-    StringBuilder message = new StringBuilder();
-    message.append("La mascota con nombre: ");
-    message.append(mascota.getNombre());
-    message.append("código postal: ");
-    message.append(mascota.getCodigoPostal());
-    message.append("y descripción: ");
-    message.append(mascota.getDescripcion());
-    message.append(" ha sido registrada correctamente.");
+    public void registrarMascota(IMascota mascota) {
+        StringBuilder message = new StringBuilder();
+        message.append("\033[0;32m");
+        message.append("La mascota con nombre: ");
+        message.append("\033[0;36m");
+        message.append(mascota.getNombre());
+        message.append("\033[0;32m");
+        message.append("código postal: ");
+        message.append("\033[0;36m");
+        message.append(mascota.getCodigoPostal());
+        message.append("\033[0;32m");
+        message.append("y descripción: ");
+        message.append("\033[0;36m");
+        message.append(mascota.getDescripcion());
+        message.append("\033[0;32m");
+        message.append(" ha sido registrada correctamente.");
+        message.append("\033[0;0m");
+        System.out.println(message);
+    }
 
-    System.out.println(message);
-  }
-
-  public void mostrarMascota(IMascota mascota) {
-    StringBuilder message = new StringBuilder();
-  }
+    public void mostrarMascota(IMascota mascota) {
+        StringBuilder message = new StringBuilder();
+    }
 }
