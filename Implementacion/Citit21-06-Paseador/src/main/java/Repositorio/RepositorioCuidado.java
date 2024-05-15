725bb67a789a4bf3574c53337352ce25f63969c2
Jorge Benjumea <jorge.benjumea@alumnos.upm.es>
2024-05-15 11:47:18 +0200

    💾 RepoCuidado

--- Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioCuidado.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/RepositorioCuidado.java
@@ -1,3 +1,38 @@
 package Repositorio;
 
-public class RepositorioCuidado {}
+import Modelo.Cuidado;
+// TODO: En el diagrama para esta clase pone <Srting> en vez de <String>
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class RepositorioCuidado {
+    public final List<Cuidado> repositorio;
+
+    public RepositorioCuidado() {
+        repositorio = new ArrayList<>();
+    }
+
+    public Cuidado crear(HashMap<String, String> cuidado) {
+        Cuidado c = new Cuidado(LocalDateTime.parse(cuidado.get("fechaCuidado")),
+                Duration.parse(cuidado.get("duracion")), Integer.parseInt(cuidado.get("id")));
+
+        repositorio.add(c);
+
+        return c;
+    }
+
+    public Cuidado obtener(int id) {
+        return repositorio.stream().filter(cuidado -> cuidado.getId() == id).findAny().orElseThrow();
+    }
+
+    public void actualizar(Cuidado cuidado) {
+
+    }
+
+    public void borrar(int id) {
+        repositorio.remove(repositorio.stream().filter(cuidado -> cuidado.getId() == id).findAny().orElseThrow());
+    }
+}
