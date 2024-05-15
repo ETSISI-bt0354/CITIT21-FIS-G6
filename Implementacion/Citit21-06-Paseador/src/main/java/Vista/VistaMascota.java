8148ba037116cd1422c99812c348de8a04abe61f
IbaiBuR <ibaibur@chessnibble.com>
2024-05-15 13:29:55 +0200

    Finish mostrarMascota and add missing method in VistaMascota

--- Implementacion/Citit21-06-Paseador/src/main/java/Vista/VistaMascota.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Vista/VistaMascota.java
@@ -20,10 +20,17 @@ public class VistaMascota {
         message.append("\033[0;32m");
         message.append(" ha sido registrada correctamente.");
         message.append("\033[0;0m");
+
         System.out.println(message);
     }
 
     public void mostrarMascota(IMascota mascota) {
         StringBuilder message = new StringBuilder();
+        message.append("Nombre: ").append(mascota.getNombre()).append("\n");
+        message.append("Descripcion: ").append(mascota.getDescripcion()).append("\n");
+
+        System.out.println(message);
     }
+
+    public void listarMascotas(IMascota mascotas) {}
 }
