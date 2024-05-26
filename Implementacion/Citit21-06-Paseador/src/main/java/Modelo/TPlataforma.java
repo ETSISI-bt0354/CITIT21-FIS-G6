1bb6eaa030c50f0bee1c1c484385e7b49beadbd6
IbaiBuR <ibaibur@chessnibble.com>
2024-05-26 18:35:53 +0200

    Uppercase in TPlataforma is a must

--- Implementacion/Citit21-06-Paseador/src/main/java/Modelo/TPlataforma.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Modelo/TPlataforma.java
@@ -9,21 +9,21 @@ public enum TPlataforma {
     MICROSOFT;
 
     public static TPlataforma parse(String data) throws PlataformaInvalida {
-        return switch (data) {
-            case "twitter" -> TPlataforma.TWITTER;
-            case "facebook" -> TPlataforma.FACEBOOK;
-            case "google" -> TPlataforma.GOOGLE;
-            case "microsoft" -> TPlataforma.MICROSOFT;
+        return switch (data.toUpperCase()) {
+            case "TWITTER" -> TPlataforma.TWITTER;
+            case "FACEBOOK" -> TPlataforma.FACEBOOK;
+            case "GOOGLE" -> TPlataforma.GOOGLE;
+            case "MICROSOFT" -> TPlataforma.MICROSOFT;
             default -> throw new PlataformaInvalida(data);
         };
     }
 
     public String toString() {
         return switch (this) {
-            case TWITTER -> "Twitter";
-            case FACEBOOK -> "Facebook";
-            case GOOGLE -> "Google";
-            case MICROSOFT -> "Microsoft";
+            case TWITTER -> "TWITTER";
+            case FACEBOOK -> "FACEBOOK";
+            case GOOGLE -> "GOOGLE";
+            case MICROSOFT -> "MICROSOFT";
         };
     }
 }
