a687ad875ab07aa322df9002aaccd9500e54d0aa
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2024-05-26 13:22:44 +0200

    Repository singleton

---
+++ Implementacion/Citit21-06-Paseador/src/main/java/Repositorio/GlobalRepository.java
@@ -0,0 +1,60 @@
+package Repositorio;
+
+import Modelo.*;
+import Serializers.*;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GlobalRepository {
+    private static GlobalRepository instance = null;
+    private final Repository<Responsable> responsables;
+    private final Repository<Cuidador> cuidadores;
+    private final Repository<Mascota> mascotas;
+    private final Repository<Exotico> exoticos;
+    private final Repository<Cuidado> cuidados;
+
+    private GlobalRepository() {
+        Path currentPath = Paths.get("").toAbsolutePath().resolve("repositorio");
+
+        try {
+            this.responsables = new Repository<>(new FileRepository<>(new SerializerXMLResponsable(), currentPath.resolve("responsables")));
+            this.cuidadores = new Repository<>(new FileRepository<>(new SerializerXMLCuidador(), currentPath.resolve("cuidadores")));
+            this.mascotas = new Repository<>(new FileRepository<>(new SerializerXMLMascota(this.responsables), currentPath.resolve("mascotas")));
+            this.exoticos = new Repository<>(new FileRepository<>(new SerializerXMLExotico(this.responsables), currentPath.resolve("exoticos")));
+            this.cuidados = new Repository<>(new FileRepository<>(new SerializerXMLCuidado(this.cuidadores, this.mascotas), currentPath.resolve("cuidados")));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static GlobalRepository getInstance() {
+        if (instance == null) {
+            instance = new GlobalRepository();
+        }
+
+        return instance;
+    }
+
+    public static Repository<Responsable> getResponsables() {
+        return getInstance().responsables;
+    }
+
+    public static Repository<Cuidador> getCuidadores() {
+        return getInstance().cuidadores;
+    }
+
+    public static Repository<Mascota> getMascotas() {
+        return getInstance().mascotas;
+    }
+
+    public static Repository<Exotico> getExoticos() {
+        return getInstance().exoticos;
+    }
+
+    public static Repository<Cuidado> getCuidados() {
+        return getInstance().cuidados;
+    }
+}
