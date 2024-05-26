db57e88b25c80bb228f19c39c080adb18b941d71
IbaiBuR <ibaibur@chessnibble.com>
2024-05-26 11:55:49 +0200

    Add parse method to TPlataforma

--- Implementacion/Citit21-06-Paseador/src/main/java/Modelo/TPlataforma.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Modelo/TPlataforma.java
@@ -5,4 +5,14 @@ public enum TPlataforma {
     FACEBOOK,
     GOOGLE,
     MICROSOFT;
+
+    public TPlataforma parse(String data) {
+        return switch (data) {
+            case "twitter" -> TPlataforma.TWITTER;
+            case "facebook" -> TPlataforma.FACEBOOK;
+            case "google" -> TPlataforma.GOOGLE;
+            case "microsoft" -> TPlataforma.MICROSOFT;
+            default -> throw new RuntimeException("Plataforma no valida.");
+        };
+    }
 }
