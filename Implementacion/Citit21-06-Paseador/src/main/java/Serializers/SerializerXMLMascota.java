953466815bebfaac8d85aa811bf3bfb740ef629d
IbaiBuR <ibaibur@chessnibble.com>
2024-05-26 12:48:46 +0200

    Append to corresponding elements in SerializerXMLMascota

--- Implementacion/Citit21-06-Paseador/src/main/java/Serializers/SerializerXMLMascota.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Serializers/SerializerXMLMascota.java
@@ -8,10 +8,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import javax.xml.transform.stream.StreamResult;
-import java.io.ByteArrayOutputStream;
-import java.io.StringWriter;
-
 public class SerializerXMLMascota implements Serializer<Mascota> {
     private final InMemoryRepository<Responsable> responsables;
 
@@ -36,12 +32,13 @@ public class SerializerXMLMascota implements Serializer<Mascota> {
         root.appendChild(codigoPostal);
 
         Element descripcion = document.createElement("descripcion");
-        nombre.appendChild(document.createTextNode(mascota.getDescripcion()));
+        descripcion.appendChild(document.createTextNode(mascota.getDescripcion()));
         root.appendChild(descripcion);
 
         Element responsable = document.createElement("responsable");
-        nombre.appendChild(document.createTextNode(String.valueOf(mascota.getResponsable().getId())));
-        root.appendChild(codigoPostal);
+        responsable.appendChild(document.createTextNode(String.valueOf(mascota.getResponsable()
+                                                                               .getId())));
+        root.appendChild(responsable);
 
         return XML.DocumentToString(document);
     }
@@ -49,15 +46,22 @@ public class SerializerXMLMascota implements Serializer<Mascota> {
     @Override
     public Mascota deserialize(String data) {
         Document document = XML.getDocument(data);
-        Node mascota = document.getElementsByTagName("responsable").item(0);
+        Node mascota = document.getElementsByTagName("responsable")
+                .item(0);
 
-        int id = Integer.parseInt(mascota.getAttributes().item(0).getNodeValue());
+        int id = Integer.parseInt(mascota.getAttributes()
+                                          .item(0)
+                                          .getNodeValue());
         NodeList nodeList = mascota.getChildNodes();
 
-        String nombre = nodeList.item(0).getTextContent();
-        int codigoPostal = Integer.parseInt(nodeList.item(1).getTextContent());
-        String descripcion = nodeList.item(2).getTextContent();
-        Responsable responsable = responsables.obtener(Integer.parseInt(nodeList.item(3).getTextContent()));
+        String nombre = nodeList.item(0)
+                .getTextContent();
+        int codigoPostal = Integer.parseInt(nodeList.item(1)
+                                                    .getTextContent());
+        String descripcion = nodeList.item(2)
+                .getTextContent();
+        Responsable responsable = responsables.obtener(Integer.parseInt(nodeList.item(3)
+                                                                                .getTextContent()));
 
         return new Mascota(id, nombre, codigoPostal, descripcion, responsable);
     }
