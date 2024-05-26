71e56b85ebf5d69c91c79b283271e7918a013b11
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2024-05-26 11:28:22 +0200

    Helper method

---
+++ Implementacion/Citit21-06-Paseador/src/main/java/Serializers/XML.java
@@ -0,0 +1,25 @@
+package Serializers;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class XML
+{
+    public static Document getDocument(String data)
+    {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = null;
+        try {
+            db = dbf.newDocumentBuilder();
+            return db.parse(new ByteArrayInputStream(data.getBytes()));
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
