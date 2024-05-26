197038e135f6de9f20470a41fc90f2be4b98471b
Miguel Ángel <ma.serrano.sanchez@alumnos.upm.es>
2024-05-26 12:09:15 +0200

    Document to String

--- Implementacion/Citit21-06-Paseador/src/main/java/Serializers/XML.java
+++ Implementacion/Citit21-06-Paseador/src/main/java/Serializers/XML.java
@@ -6,7 +6,13 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class XML
@@ -30,5 +36,21 @@ public class XML
         } catch (ParserConfigurationException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String DocumentToString(Document document) {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        StreamResult result = new StreamResult(output);
+
+        DOMSource source = new DOMSource(document);
+
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
+        }
+
+        return output.toString();
     }
 }
